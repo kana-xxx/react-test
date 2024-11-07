91a8b381376dd02ec5532f7c1cd02488eb32c8af
kana-xxx <k.konishi@env-value.co.jp>
2024-11-08 08:25:43 +0900

    Deleted unnecessary files

--- Cloudus/js/header-margin.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // ヘッダーの要素を取得
-    var header = document.querySelector('header');
-    // ヘッダーの高さを取得
-    var headerHeight = header.offsetHeight;
-    // ヘッダーの高さ分だけコンテンツを下に移動
-    document.body.style.paddingTop = headerHeight + 'px' ;
-});
--- Cloudus/js/humburgerMenu.js
+++
@@ -1,9 +0,0 @@
-const hamburgerMenu = document.querySelector('.menu.on');
-const hamburgerList = document.querySelector('.subList');
-const Bg = document.querySelector('html');
-hamburgerMenu.addEventListener('click', () => {
-hamburgerMenu.classList.toggle('active');
-hamburgerList.classList.toggle('active');
-Bg.classList.toggle('scroll-prevent') // 追記
-});
-
