7e00d9f38da6b3eb4201e2a1275558987c4a403e
kana-xxx <k.konishi@env-value.co.jp>
2024-11-08 08:22:46 +0900

    second commit

--- react-test/src/app/page.js
+++ react-test/src/app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
             Get started by editing <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>テストのため追加</li>
         </ol>
 
         <div className={styles.ctas}>
